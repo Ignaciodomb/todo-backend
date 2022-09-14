bab3cfe6eacfd6999374294b3f1cb7573e43b036
Ignacio Dominguez <ignacio.dominguez@upa.edu.py>
2022-09-14 08:49:45 -0400

    Primer commit

---
+++ models/task.py
@@ -0,0 +1,18 @@
+import json
+
+
+class TaskModel():
+    id = int
+    description = str
+
+    def __init__(self, id, description):
+        self.id = id
+        self.description = description
+    
+    def json(self, depth =0):
+        json = {
+            'id': self.id,
+            'description': self.description
+        }
+
+        return json
